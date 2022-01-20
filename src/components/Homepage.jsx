a17d23bb64941c35200dfb253f959c3ac01d66ac
Syfax Acherfouche <syfax15_98@outlook.fr>
2022-01-20 01:04:25 +0100

    Homepage without api

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import millify from 'millify'
+import { Typography, Row, Col, Stat, Statistic} from 'antd'
 
+import { useGetCryptosQuery } from '../services/cryptoApi';
+const { Title } = Typography
 const Homepage = () => {
+    // const { data, isFetching } = useGetCryptosQuery()
+    // const globalStats = data?.data?.stats 
+    // console.log(globalStats)
+
+    // if(isFetching) return 'Loading... '
     return (
-        <div>
-            Homepage
-        </div>
+        <>
+            <Title level={2} className='heading'>
+                Global Crypto Stats 
+            </Title> 
+            <Row>
+                <Col span={12}><Statistic title='Total Cryptocurrencies' value={10002}/></Col>
+                <Col span={12}><Statistic title='Total Exchanges' value={50}/></Col>
+                <Col span={12}><Statistic title='Total Market Cap' value={15}/></Col>
+                <Col span={12}><Statistic title='Total 24h Volume' value={50000}/></Col>
+                <Col span={12}><Statistic title='Total Markets' value={5}/></Col>
+            </Row>
+        </>
     )
 }
 
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '2c7dd4ab63mshdacf1de5eb6c970p11a770jsnc73d71b2eace'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/exchange/-zdvbieRdZ/coins'
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = createApi
